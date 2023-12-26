3e06bf3997ba50ee12002b6226d7ac0e00fe41d0
handongqingxue <1417140290@qq.com>
2023-12-26 16:49:22 +0800

    1

--- src/main/java/com/batchClientHL/controller/HomeController.java
+++ src/main/java/com/batchClientHL/controller/HomeController.java
@@ -1,5 +1,19 @@
 package com.batchClientHL.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
 
+	public static final String MODULE_NAME="home";
+	
+	@RequestMapping(value="/recipeManager")
+	public String goRecipeManager(HttpServletRequest request) {
+
+		return MODULE_NAME+"/recipeManager";
+	}
 }
